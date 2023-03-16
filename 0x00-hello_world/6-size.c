79a33f044cffaf15f0125fd6092c48cc7aea8272
Samuel Osoro <slovinwriter@gmail.com>
2023-03-16 11:57:13 -0700

    szie is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,18 @@
 
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)", sizeof(char));
-	printf("Size of int: %lu byte(s)", sizeof(int));
-	printf("Size of a long int: %lu byte(s)", sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
-	printf("Size of a float: %lu byte(s)", sizeof(float));
+	int intType;
+	float floatType;
+	long longType;
+	long long longlongintType;
+	char charType;
+
+	// sizeof refers the size of a variable
+	printf("Size of a char: %lu byte\n", sizeof(charType));
+	printf("Size of int: %lu byte\n", sizeof(intType));
+	printf("Size of a long int: %lu byte\n", sizeof(longintTypr));
+	printf("Size of a long long int: %lu byte\n", sizeof(longlongintType));
+	printf("Size of a float: %lu byte(s)", sizeof(floatType));
 	return (0);
 }
 
