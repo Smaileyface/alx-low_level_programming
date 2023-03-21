5e1cff72fb1d7003787041026f62cb5c261c8b71
Samuel Osoro <slovinwriter@gmail.com>
2023-03-21 12:02:23 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,11 @@
-#include<stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point
- * Return: Always 0
+ * main - print _putchar, followed by a newline
+ * Return: Always 0 (success)
  */
-
 int main(void)
-
 {
-
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -18,6 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return (0);
 
+	return (0);
 }
