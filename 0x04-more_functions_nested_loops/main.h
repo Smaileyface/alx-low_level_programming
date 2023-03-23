129b5dd441dd330e40a8bd56a1ca8846b521ecdb
Samuel Osoro <slovinwriter@gmail.com>
2023-03-23 15:05:00 -0700

    0.isupper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- * File: main.h
- *
- * Desc: header file
-**/
-
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -18,7 +12,5 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-ivoid print_number(int n);
-
-#endif /* MAIN_H */
-
+void print_number(int n);
+#endif
