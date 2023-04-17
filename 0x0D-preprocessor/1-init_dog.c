4fba21e6ceb31a690c25343917de65a88ea7933d
Samuel Osoro <slovinwriter@gmail.com>
2023-04-17 13:30:53 -0700

    1

--- 0x0D-preprocessor/1-init_dog.c
+++ 0x0D-preprocessor/1-init_dog.c
@@ -1,20 +1,19 @@
-#include "dog.h"
-#include <stdio.h>
-/**
- * init_dog - a function that initializes variable
- * of type struct dog
- * @d:pointer to struct dog
- * @name:pointer to a string
- * @age:float
- * @owner:pointer to a string
- */
-void init_dog(struct dog *d, char *name, float age, char *owner);
-{
-	if (d != NULL)
+#include <stdlib.h>
+	#include "dog.h"
+
+
+	/**
+	 * init_dog - initializes a variable of type struct dog
+	 * @d: pointer to struct dog to initialize
+	 * @name: name to initialize
+	 * @age: age to initialize
+	 * @owner: owner to initialize
+	 */
+	void init_dog(struct dog *d, char *name, float age, char *owner)
 	{
-	d = malloc(sizeof(structdog));
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+		if (d == NULL)
+			d = malloc(sizeof(struct dog));
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
-}
