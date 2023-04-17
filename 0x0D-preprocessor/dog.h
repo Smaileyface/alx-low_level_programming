1b7a6958ffb762ed9592aae71ea551af22c82029
Samuel Osoro <slovinwriter@gmail.com>
2023-04-17 13:37:40 -0700

    0

--- 0x0D-preprocessor/dog.h
+++ 0x0D-preprocessor/dog.h
@@ -1,18 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
- * struct dog - a dog's info
+ * struct dog - a dog's basic info
  * @name: First member
  * @age: Second member
  * @owner: Third member
  *
- * Description: A dog with a name, age and owner
+ * Description: Longer description
  */
 struct dog
 {
-	char*name;
+	char *name;
 	float age;
-	char*owner;
+	char *owner;
 };
 
 /**
@@ -24,4 +25,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
 #endif
